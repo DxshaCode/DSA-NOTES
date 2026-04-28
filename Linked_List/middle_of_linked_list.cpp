1ead1bed084dbe9e244dabfbfe1ddb41d9a01aba
DxshaCode <diksha10211@gmail.com>
2026-04-29 00:52:03 +0530

    Create middle_of_linked_list.cpp

---
+++ Linked_List/middle_of_linked_list.cpp
@@ -0,0 +1,9 @@
+//problem: middle of the linked list
+//concept: hare and tortoise algo. 
+        //it has two pointers one fast and one slow.slow moves one step and fast moves 2 steps.
+
+//approach: when fast pointer moves to the end slow pointer will be  at midlle so in one traversal only we can find the middle node of a linked list.
+
+//time complexity: O(n);
+//space complexity: O(1)
+
