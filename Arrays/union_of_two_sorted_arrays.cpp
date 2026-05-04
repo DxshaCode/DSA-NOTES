cb45bd3fa02ee5d1f588bfe917fad8cc43492dd9
DxshaCode <diksha10211@gmail.com>
2026-05-04 13:40:14 +0530

    Update union_of_two_sorted_arrays.cpp

--- Arrays/union_of_two_sorted_arrays.cpp
+++ Arrays/union_of_two_sorted_arrays.cpp
@@ -34,5 +34,54 @@ class Solution {
 // ##optimal approach:
 //concept: two pointers
 
+// time complexity:O(m+n)
+// space complexity:O(m+n)
+
+class Solution {
+  public:
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        int i = 0, j = 0;
+        vector<int> res;
+
+        while(i < a.size() && j < b.size())
+        {
+            if(a[i] == b[j])
+            {
+                if(res.empty() || res.back() != a[i]) //nya vector empty hoga to insert krna hai or agr wo empty nahi ho or nye vector ka last element diff ho to inserrt kro
+                    res.push_back(a[i]);
+                i++; //or agr wo dono equal hai or vector empty bhi nahi hai pr last  element same hai to ofc we have to skip those numbers so we will inc. i and j
+                j++;
+            }
+            else if(a[i] < b[j])
+            {
+                if(res.empty() || res.back() != a[i])
+                    res.push_back(a[i]);
+                i++;
+            }
+            else
+            {
+                if(res.empty() || res.back() != b[j])
+                    res.push_back(b[j]);
+                j++;
+            }
+        }
+
+        while(i < a.size())
+        {
+            if(res.empty() || res.back() != a[i])
+                res.push_back(a[i]);
+            i++;
+        }
+
+        while(j < b.size())
+        {
+            if(res.empty() || res.back() != b[j])
+                res.push_back(b[j]);
+            j++;
+        }
+
+        return res;
+    }
+};
 
 
